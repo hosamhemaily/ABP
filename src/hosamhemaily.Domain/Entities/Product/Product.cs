5da2dfd815d6e6c52a1a9518f6d6461d1248bb12
HYehia <HYehia@HYEHIA-LT>
2024-10-12 16:24:08 +0300

    category

---
+++ src/hosamhemaily.Domain/Entities/Product/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace hosamhemaily.Entities.PriceOffer
+{
+    public class Product : FullAuditedAggregateRoot<Guid>
+    {
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public Guid CategoryId { get; set; }
+
+        public virtual Category.Category Category { get; set; }
+
+    }
+}
